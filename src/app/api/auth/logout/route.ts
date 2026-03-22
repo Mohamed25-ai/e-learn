a6558705a0b59900e677be37a7e22b6bfb5eb5db
Mohamed25 <mohamed25@example.com>
2026-03-22 04:45:28 +0200

    e-learn1

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 
 export  async  function GET() {
     const response = NextResponse.rewrite('/login');
-     response.cookies.delete('next-auth.session-token');
+     response.cookies.delete('next-auth.session-token')||response.cookies.delete('__Secure-next-auth.session-token');
     return response
 }
